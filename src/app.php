d419adbd037e1a879891bc230383a1910e448c36
stas <stas@debian.debian>
2023-02-04 14:15:48 +0300

    sending email

--- src/app.php
+++ src/app.php
@@ -16,10 +16,21 @@ if(!$is_isset) {
 
 if ($is_isset) {
   $PROCESSES = (int) $_ENV["PROCESSES"];
-
   do {
     $process = new React\ChildProcess\Process('php '.__DIR__.'/process/amqp.php');
     $process->start();
+
+    $process->stdout->on('data', function ($chunk) {
+      echo $chunk;
+    });
+
+    $process->stdout->on('error', function (Exception $e) {
+      echo 'error: ' . $e->getMessage();
+    });
+
+    $process->on('exit', function($exitCode, $termSignal) {
+      echo 'Process exited with code ' . $exitCode . PHP_EOL;
+    });
 
     $PROCESSES = $PROCESSES - 1;
   } while ($PROCESSES > 0);
--- src/process/amqp.php
+++ src/process/amqp.php
@@ -19,8 +19,6 @@ $connection = [
   'password' => $_ENV["RB_PASSWORD"],
 ];
 
-$mail = new PHPMailer(true);
-
 $bunny = new Client($connection);
 $bunny->connect();
 
@@ -28,10 +26,76 @@ $channel = $bunny->channel();
 $channel->queueDeclare($_ENV["NOTIFYING_QUEUE"]);
 $channel->queueDeclare($_ENV["LOGGING_QUEUE"]);
 
+function publish($args)
+{
+  $channel->publish(json_encode([
+    "type" => $args["type"],
+    "message" => $args["message"],
+    "date" => date("Y-m-d H:i:s")
+  ]), [], "", $_ENV["LOGGING_QUEUE"]);
+}
+
+function send_email($to, $subject, $message)
+{
+  $mail = new PHPMailer(true);
+  $mail->CharSet = "UTF-8";
+
+  $mail->isSMTP();
+  $mail->Host       = $_ENV["SMTP_HOST"];
+  $mail->SMTPAuth   = true;
+  $mail->Username   = $_ENV["SMTP_USER"];
+  $mail->Password   = $_ENV["SMTP_PASSWORD"];
+  $mail->Port       = $_ENV["SMTP_PORT"];
+
+  $mail->setFrom($_ENV["SMTP_HOST"], $_ENV["SMTP_HOST"]);
+  $mail->addAddress($to);
+
+  $mail->isHTML(true);
+  $mail->Subject = $subject;
+  $mail->Body = $message;
+
+  $mail->send();
+}
+
 $channel->run(
   function (Message $message, Channel $channel, Client $bunny) {
-      print_r($message);
-      $channel->ack($message);
+    $obj = json_decode($message->{'content'});
+    if(!is_object($obj)) {
+      publish([
+        "type" => "info",
+        "message" => "Invalid json. Queue - {$_ENV['LOGGING_QUEUE']}. Id application - {$_ENV['ID_APPLICATION']}. Payload - {$message->{'content'}}"
+      ]);
+    }
+
+    if (is_object($obj)) {
+      if(!$obj->{'email'} || !$obj->{'subject'} || !$obj->{'message'}) {
+        publish([
+          "type" => "info",
+          "message" => "Invalid json. Queue - {$_ENV['LOGGING_QUEUE']}. Id application - {$_ENV['ID_APPLICATION']}. Payload - {$message->{'content'}}"
+        ]);
+      }
+
+      if ($obj->{'email'} && $obj->{'subject'} && $obj->{'message'}) {
+        $result_validation = filter_var($obj->{'email'}, FILTER_VALIDATE_EMAIL);
+
+        if (!$result_validation) {
+          publish([
+            "type" => "info",
+            "message" => "Invalid email address. Queue - {$_ENV['LOGGING_QUEUE']}. Id application - {$_ENV['ID_APPLICATION']}. Payload - {$message->{'content'}}"
+          ]);
+        }
+
+        if ($result_validation) {
+          send_email($obj->{'email'}, $obj->{'subject'}, $obj->{'message'});
+
+          publish([
+            "type" => "info",
+            "message" => "Sent email. Queue - {$_ENV['LOGGING_QUEUE']}. Id application - {$_ENV['ID_APPLICATION']}. Payload - {$message->{'content'}}"
+          ]);
+        }
+      }
+    }
+    $channel->ack($message);
   },
   $_ENV["NOTIFYING_QUEUE"]
 );
